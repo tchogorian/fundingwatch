32c4a231a937bc3506b1d02253ad8858c7bfd78e
FundingWatch <alex@fundingwatch.org>
2026-03-09 19:58:01 -0400

    blog: publish 10 articles\n\n- lendora-mca-contract-review\n- coastal-capital-mca-contract-review\n- mca-laws-new-york-laws-overview\n- mca-laws-new-york-recent-enforcement-actions\n- what-happens-if-you-stop-paying-your-mca\n- can-my-mca-lender-freeze-my-bank-account\n- what-is-confession-of-judgment-mca\n- yellowstone-capital-534m-settlement-explained\n- mca-vs-sba-loan-which-is-right-for-your-business\n- how-to-rebuild-cash-flow-after-an-mca

--- src/lib/blog-posts/index.ts
+++ src/lib/blog-posts/index.ts
@@ -11,6 +11,17 @@ import { post as mcaIndustrySpecificGuide } from "./mca-industry-specific-guide"
 import { post as businessDebtRestructuringMca } from "./business-debt-restructuring-mca";
 import { post as freeMcaContractAnalysis } from "./free-mca-contract-analysis";
 
+import { post as lendoraMcaContractReview } from "./lendora-mca-contract-review";
+import { post as coastalCapitalMcaContractReview } from "./coastal-capital-mca-contract-review";
+import { post as mcaLawsNewYorkLawsOverview } from "./mca-laws-new-york-laws-overview";
+import { post as mcaLawsNewYorkRecentEnforcementActions } from "./mca-laws-new-york-recent-enforcement-actions";
+import { post as whatHappensIfYouStopPayingYourMca } from "./what-happens-if-you-stop-paying-your-mca";
+import { post as canMyMcaLenderFreezeMyBankAccount } from "./can-my-mca-lender-freeze-my-bank-account";
+import { post as whatIsConfessionOfJudgmentMca } from "./what-is-confession-of-judgment-mca";
+import { post as yellowstoneCapital534mSettlementExplained } from "./yellowstone-capital-534m-settlement-explained";
+import { post as mcaVsSbaLoanWhichIsRightForYourBusiness } from "./mca-vs-sba-loan-which-is-right-for-your-business";
+import { post as howToRebuildCashFlowAfterAnMca } from "./how-to-rebuild-cash-flow-after-an-mca";
+
 export const PILLAR_POSTS = [
   mcaDebtReliefGuide,
   merchantCashAdvanceDefense,
@@ -24,4 +35,14 @@ export const PILLAR_POSTS = [
   mcaIndustrySpecificGuide,
   businessDebtRestructuringMca,
   freeMcaContractAnalysis,
+  lendoraMcaContractReview,
+  coastalCapitalMcaContractReview,
+  mcaLawsNewYorkLawsOverview,
+  mcaLawsNewYorkRecentEnforcementActions,
+  whatHappensIfYouStopPayingYourMca,
+  canMyMcaLenderFreezeMyBankAccount,
+  whatIsConfessionOfJudgmentMca,
+  yellowstoneCapital534mSettlementExplained,
+  mcaVsSbaLoanWhichIsRightForYourBusiness,
+  howToRebuildCashFlowAfterAnMca,
 ];
