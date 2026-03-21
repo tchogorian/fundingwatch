c17d16d8574e4f78697aaa862f4bdb4b4f6f32cf
FundingWatch <alex@fundingwatch.org>
2026-03-20 23:28:54 -0400

    Remove 7 scary/defensive articles — site should attract borrowers not repel them

--- src/lib/blog-posts/index.ts
+++ src/lib/blog-posts/index.ts
@@ -3,12 +3,10 @@ import { post as complaintAnalysisResponsibleVsPredatory } from "./1700-borrower
 import { post as mcaDebtReliefGuide } from "./mca-debt-relief-guide";
 import { post as merchantCashAdvanceDefense } from "./merchant-cash-advance-defense";
 import { post as howToAnalyzeMcaContract } from "./how-to-analyze-mca-contract";
-import { post as mcaDefaultConsequences } from "./mca-default-consequences";
 import { post as mcaLawsByState } from "./mca-laws-by-state";
 import { post as mcaAlternatives } from "./mca-alternatives";
 import { post as mcaLenderProfilesOverview } from "./mca-lender-profiles-overview";
 import { post as smallBusinessCashFlowMca } from "./small-business-cash-flow-mca";
-import { post as mcaScamsPredatoryLenders } from "./mca-scams-predatory-lenders";
 import { post as mcaIndustrySpecificGuide } from "./mca-industry-specific-guide";
 import { post as businessDebtRestructuringMca } from "./business-debt-restructuring-mca";
 import { post as freeMcaContractAnalysis } from "./free-mca-contract-analysis";
@@ -17,18 +15,13 @@ import { post as lendoraMcaContractReview } from "./lendora-mca-contract-review"
 import { post as coastalCapitalMcaContractReview } from "./coastal-capital-mca-contract-review";
 import { post as mcaLawsNewYorkLawsOverview } from "./mca-laws-new-york-laws-overview";
 import { post as mcaLawsNewYorkRecentEnforcementActions } from "./mca-laws-new-york-recent-enforcement-actions";
-import { post as canMyMcaLenderFreezeMyBankAccount } from "./can-my-mca-lender-freeze-my-bank-account";
 import { post as mcaVsSbaLoanWhichIsRightForYourBusiness } from "./mca-vs-sba-loan-which-is-right-for-your-business";
 
-import { post as myMcaHas300PercentAprIsThatLegal } from "./my-mca-has-300-percent-apr-is-that-legal";
 import { post as whatIsPersonalGuaranteeMca } from "./what-is-personal-guarantee-mca";
-import { post as cfpbMerchantCashAdvanceCrackdown } from "./cfpb-merchant-cash-advance-crackdown";
 
 import { post as regalCapitalMcaContractReview } from "./regal-capital-mca-contract-review";
 import { post as kapitusMcaContractReview } from "./kapitus-mca-contract-review";
-import { post as iDefaultedOnMyMcaWhatAreMyOptions } from "./i-defaulted-on-my-mca-what-are-my-options";
 
-import { post as merchantCashAdvancesSmallBusinessBankruptcies } from "./merchant-cash-advances-small-business-bankruptcies";
 import { post as mcaLawsNewYorkBorrowerRightsAndRemedies } from "./mca-laws-new-york-borrower-rights-and-remedies";
 import { post as mcaIndustry20BillionMarketBorrowers } from "./mca-industry-20-billion-market-borrowers";
 import { post as beforeSigningMca10Things } from "./before-signing-mca-10-things";
